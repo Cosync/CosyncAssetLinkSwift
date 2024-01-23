dbab88b7cc8eab65bd8fe177f1e1b85e507d149f
Collin Davidson <fcdavidson@gmail.com>
2024-01-22 20:19:23 -0500

    Refactor AssetPicker to fix multi select

--- Sources/CosyncAssetLinkSwift/CSAssetPicker.swift
+++ Sources/CosyncAssetLinkSwift/CSAssetPicker.swift
@@ -26,28 +26,35 @@ import Foundation
 import PhotosUI
 import SwiftUI
 
+
+
+public struct AssetPickerResult {
+    
+    public var success: Bool = true
+    public var error: String = ""
+    public var assetId: String = ""
+    public var assetType: String = ""
+    public var data: Any // as UIImage or URL
+}
+
+public typealias AssetPickerCallback = (_ result: AssetPickerResult) -> Void
+
 @available(iOS 15, *)
 public struct AssetPicker: UIViewControllerRepresentable {
     
-    @Binding var pickerResult: [String]
-    @Binding var selectedImage:UIImage?
-    @Binding var selectedVideoUrl:URL?
-    @Binding var selectedType:String
     @Binding var isPresented: Bool
-    @Binding var errorMessage:String?
     var preferredType:String = "all"
     var isMultipleSelection:Bool = false
+    var onPicked: AssetPickerCallback
      
-    public init(pickerResult:Binding<[String]>, selectedImage:Binding<UIImage?>, selectedVideoUrl:Binding<URL?>,
-                selectedType:Binding<String>, isPresented:Binding<Bool>, errorMessage:Binding<String?>, preferredType:String, isMultipleSelection:Bool) {
-        self._pickerResult = pickerResult
-        self._selectedImage = selectedImage
-        self._selectedVideoUrl = selectedVideoUrl
-        self._selectedType = selectedType
+    public init(isPresented:Binding<Bool>,
+                preferredType:String,
+                isMultipleSelection:Bool,
+                onPicked: @escaping AssetPickerCallback) {
         self._isPresented = isPresented
-        self._errorMessage = errorMessage
         self.preferredType = preferredType
         self.isMultipleSelection = isMultipleSelection
+        self.onPicked = onPicked
     }
     
     
@@ -84,6 +91,66 @@ public struct AssetPicker: UIViewControllerRepresentable {
         Coordinator(self)
     }
     
+    public func onPickedAsset(pickedAsset: PHPickerResult) {
+        
+        let provider = pickedAsset.itemProvider
+        var result = AssetPickerResult(success: false, error: "", assetId: "", assetType: "", data: "")
+        
+        if provider.hasItemConformingToTypeIdentifier(UTType.movie.identifier) {
+            let progress:Progress = provider.loadFileRepresentation(forTypeIdentifier: UTType.movie.identifier) { fileURL, err in
+                do {
+                    if let url = fileURL {
+                        let fm = FileManager.default
+                        let filename = url.lastPathComponent
+                        let destination = fm.temporaryDirectory.appendingPathComponent(filename)
+                        if fm.fileExists(atPath: destination.path) {
+                            try fm.removeItem(at: destination)
+                        }
+                        
+                        try fm.copyItem(at: url, to: destination)
+                        result = AssetPickerResult(success: true, error: "", assetId: pickedAsset.assetIdentifier ?? "", assetType: "video", data: destination)
+                        
+                        self.onPicked(result)
+                    }
+                    else {
+                        result.error = "Unable to load video"
+                        self.onPicked(result)
+                    }
+                }
+                catch {
+                    result.error = "Unable to load video"
+                    self.onPicked(result)
+                }
+            }
+            
+            print("load progress \(String(describing: progress.estimatedTimeRemaining))")
+        }
+        else if provider.hasItemConformingToTypeIdentifier(UTType.image.identifier) {
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                
+                provider.loadObject(ofClass: UIImage.self, completionHandler: { (object, error) in
+                     
+                    if let err = error {
+                        result.error  = err.localizedDescription
+                    }
+                    else if let image = object as? UIImage {
+                        result = AssetPickerResult(success: true, error: "", assetId: pickedAsset.assetIdentifier ?? "", assetType: "image", data: image)
+                        self.onPicked(result)
+                    }
+                })
+                
+            }
+            else {
+                result.error  = "Can not load this image."
+                self.onPicked(result)
+            }
+        }
+        else {
+            result.error = "Unsupported type"
+            self.onPicked(result)
+        }
+    }
+    
     /// PHPickerViewControllerDelegate => Coordinator
     public class Coordinator: PHPickerViewControllerDelegate {
         
@@ -93,89 +160,17 @@ public struct AssetPicker: UIViewControllerRepresentable {
             self.parent = parent
         }
         
-       
             
         public func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            
             picker.dismiss(animated: true, completion: nil)
             
-            var assetIdList = [String]()
-           
-            guard let provider = results.first?.itemProvider else { return }
-            
-            if provider.hasItemConformingToTypeIdentifier(UTType.movie.identifier) {
-                
-                for asset in results {
-                    if let assetId = asset.assetIdentifier {
-                        assetIdList.append(assetId)
-                    }
-                     
-                }
-                parent.pickerResult = assetIdList
-                let progress:Progress = provider.loadFileRepresentation(forTypeIdentifier: UTType.movie.identifier) { fileURL, err in
-                //provider.loadFileRepresentation(forTypeIdentifier: UTType.movie.identifier, options: [:]) { [self] (videoURL, error) in
-                    do {
-                        if let url = fileURL {
-                            let fm = FileManager.default
-                            let filename = url.lastPathComponent
-                            let destination = fm.temporaryDirectory.appendingPathComponent(filename)
-                            if fm.fileExists(atPath: destination.path) {
-                                try fm.removeItem(at: destination)
-                            }
-                            
-                            try fm.copyItem(at: url, to: destination)
-                            self.parent.selectedVideoUrl = destination
-                            
-                        }
-                        else {
-                            self.parent.errorMessage = "Can not load this video."
-                        }
-                        self.parent.selectedType = "video"
-                    }
-                    catch{
-                        self.parent.errorMessage = "Can not load this video."
-                    }
-                }
-                
-                print("load progress \(String(describing: progress.estimatedTimeRemaining))")
-            }
-            else if provider.hasItemConformingToTypeIdentifier(UTType.image.identifier) {
-                
-                self.parent.selectedType = "image"
-                
-                for asset in results {
-                    
-                    if asset.itemProvider.canLoadObject(ofClass: UIImage.self) {
-                        
-                        asset.itemProvider.loadObject(ofClass: UIImage.self, completionHandler: { (object, error) in
-                             
-                            if let err = error {
-                                self.parent.errorMessage = err.localizedDescription
-                            }
-                            else if let image = object as? UIImage {
-                                
-                                self.parent.selectedImage = image
-                                
-                                if let assetId = asset.assetIdentifier {
-                                    assetIdList.append(assetId)
-                                }
-                                
-                                self.parent.pickerResult = assetIdList
-                            }
-                        })
-                        
-                    }
-                    else {
-                        self.parent.errorMessage = "Can not load this image."
-                    }
-                }
-
-                parent.pickerResult = assetIdList
+            for result in results {
+                parent.onPickedAsset(pickedAsset: result)
             }
             
-            
-                  
-          // dissmiss the picker
-          parent.isPresented = false
+            // dissmiss the picker
+            parent.isPresented = false
         }
     }
 }
